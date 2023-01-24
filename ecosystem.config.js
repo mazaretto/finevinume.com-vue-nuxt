c8def0eb7bd69bd99fd4eff68d8a0f750cf53adb
Алексей Ларионов <alexey.larionov.fl@gmail.com>
2023-01-25 00:22:22 +0700

    deploy update

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,4 +1,5 @@
 module.exports = {
+  basePath: 'http://finevinume.com',
   apps: [
     {
       name: 'FineVinum',
@@ -7,16 +8,5 @@ module.exports = {
       script: './node_modules/nuxt/bin/nuxt.js',
       args: 'start'
     },
-  ],
-  deploy : {
-    production : {
-      user: 'admin',
-      host: ['45.67.57.171'],
-      ref: 'origin/master',
-      repo: 'git@github.com:mazaretto/finevinume.com-vue-nuxt.git',
-      ssh_options: ['ForwardAgent=yes'],
-      path: '/home/admin/web/finevinume.com/public_html',
-      'post-deploy' : 'npm && npm build && pm2 startOrRestart ecosystem.config.js --env production'
-    }
-  }
+  ]
 }
