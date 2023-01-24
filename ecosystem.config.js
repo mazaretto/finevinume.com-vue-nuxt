dbfed9b346151c620df3870fb7695354966fc64d
Алексей Ларионов <alexey.larionov.fl@gmail.com>
2023-01-24 23:58:33 +0700

    deploy update

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,6 +6,16 @@ module.exports = {
       instances: 'max', // Or a number of instances
       script: './node_modules/nuxt/bin/nuxt.js',
       args: 'start'
+    },
+  ],
+  deploy : {
+    production : {
+      user: 'admin',
+      host: ['machine ip address'],
+      ref: 'origin/master',
+      ssh_options: ['ForwardAgent=yes'],
+      path: '/home/admin/web/finevinume.com/public_html',
+      'post-deploy' : 'npm && npm build && pm2 startOrRestart ecosystem.config.js --env production'
     }
-  ]
+  }
 }
