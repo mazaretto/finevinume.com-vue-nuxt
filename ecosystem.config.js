dcb7168fb7342869e79aa61bc83f74e94591fe66
Алексей Ларионов <alexey.larionov.fl@gmail.com>
2023-01-25 00:00:52 +0700

    deploy update

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -13,6 +13,7 @@ module.exports = {
       user: 'admin',
       host: ['machine ip address'],
       ref: 'origin/master',
+      repo: 'git@github.com:mazaretto/finevinume.com-vue-nuxt.git',
       ssh_options: ['ForwardAgent=yes'],
       path: '/home/admin/web/finevinume.com/public_html',
       'post-deploy' : 'npm && npm build && pm2 startOrRestart ecosystem.config.js --env production'
